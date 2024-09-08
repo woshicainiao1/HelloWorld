9485371e5c99f293422cd0634651cc0e34a9828c
woshicainiao1 <1458239040@qq.com>
2024-09-08 23:40:47 +0800

    更新 HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 int main()
 {
-    std::cout << "Hello, GitHub!" << std::endl;
+<<<<<<< HEAD
+    std::cout << "Hello, GitHub!！" << std::endl;
+=======
+    std::cout << "Hello, GitHub Desktop!" << std::endl;
+>>>>>>> 23f83c5d61a4689426e94e7c6d594c9f390eb577
     return 0;
 }
