2e00116194bdfbd2ae06c7042b16eaf1d644127e
woshicainiao1 <1458239040@qq.com>
2025-02-03 14:06:34 +0800

    回退到1.4

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,6 +2,10 @@
 
 int main()
 {
-    std ::cout << "HelloWorld-1.5" << std ::endl;
+    std ::cout << "HelloWorld-1.4" << std ::endl;
     return 0;
 }
+
+The above code is a simple C++ program that prints ¡°HelloWorld - 1.4¡± to the console.
+Step 2: Create a Dockerfile
+Now, create a Dockerfile in the same directory as the HelloWorld.cpp file.
